01df5db07048410f507b99f1a08827b3dc72bfaa
Roman Naumenko <naumenko.roman@gmail.com>
2025-01-13 11:59:44 +0200

    Propagated previous feature up.

--- network/__init__.py
+++ network/__init__.py
@@ -1,11 +1,11 @@
 from .models import SegFaceLapa, SegFaceCeleb, SegFaceHelen
 
 
-def get_model(backbone, input_resolution, model, half=False):
+def get_model(backbone, input_resolution, model, half=False, backbone_only=False):
     if backbone == "segface_lapa":
         model = SegFaceLapa(input_resolution, model)
     elif backbone == "segface_celeb":
-        model = SegFaceCeleb(input_resolution, model, half)
+        model = SegFaceCeleb(input_resolution, model, half, backbone_only)
     elif backbone == "segface_helen":
         model = SegFaceHelen(input_resolution, model)
     else:
